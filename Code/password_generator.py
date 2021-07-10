48fa3e01b73f11dfca943dfef2fa7afcce97dfcf
datta-agni <dattadunga@gmail.com>
2021-07-10 22:46:05 +0530

    Updated.
    Added randomization algorithm to increase complexity of passwords.

--- Code/password_generator.py
+++ Code/password_generator.py
@@ -1,19 +1,25 @@
 # Password generator to generate strong customs passwords. This will create random passwords of different length.
 
 import random
+from more_itertools import random_permutation
 
 # actual function which generates the password
 
 
 def generatePassword(password_length):
+
     # initialzing the string arrays which contains the possible charecters which can be used in a password.
     small_alphabet = "abcdefghijklmnopqrstuvwxyz"
     capital_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    symbols = "!@#$%^&*()_+=-[]{;}:~`>/<"
-    alphabet = symbols + small_alphabet + capital_alphabet
+    symbols = "!@#$%^&*()_+=-[]{/}><"
+    numbers = "0123456789"
+    alphabet = numbers + small_alphabet + symbols + capital_alphabet
     passwords = []
 
-# creating a function to create and return the the password.
+    # shuffles the alphabet string with random permutation this increases randomization
+    ''.join(random_permutation(alphabet))
+
+    # creating a function to create and return the the password.
     for i in password_length:
 
         password = ""
@@ -21,29 +27,15 @@ def generatePassword(password_length):
             next_letter_index = random.randrange(len(alphabet))
             password = password + alphabet[next_letter_index]
 
-        password = replaceWithNumber(password)
         passwords.append(password)
 
     return passwords
-
-# function which replaces the words in the generated password and replaces them which
-
-
-def replaceWithNumber(pass_world):
-
-    # generates and appends random numbers and returns it.
-
-    for i in range(random.randrange(1, 3)):
-        replace_index = random.randrange(len(pass_world)//2)
-        pass_world = pass_world[0:replace_index] + \
-            str(random.randrange(10)) + pass_world[replace_index+1:]
-        return pass_world
-
 
 # main function which takes the user input for the following parameters:
 # number of passwords to be generated
 # length of the password aka strength
 # prints the password in plain-text form
+
 
 def main():
 
